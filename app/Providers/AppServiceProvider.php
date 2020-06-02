c349b238d886371b36efea3a68ff0185ee397d65
Ripunjoy <ripunjoy.skaplink@gmail.com>
2020-06-02 02:29:12 -0700

    Tsk

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //For MySQL older than the 5.7.7 
+        Schema::defaultStringLength(191);
+        Validator::extend('recaptcha', 'App\\Validators\\ReCaptcha@validate');
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //factory(App\User::class, 2)->create();
+
+        $this->call([ CountriesSeeder::class,]);
+        $this->call([ UserRoleSeeder::class,]);
+    }
+}
